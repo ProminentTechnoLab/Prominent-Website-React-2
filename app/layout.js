b879dac3620700147ef7fd8c7b6ffc9550315e8c
thescriptforge <thescriptforge@gmail.com>
2026-03-14 16:03:21 +0530

    Meta Pixel Code added

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import { Geist } from 'next/font/google'
+import Script from 'next/script'
+
 import './globals.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
@@ -72,7 +74,35 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className={geist.variable}>
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1007984794652617');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1007984794652617&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <div className="app-wrapper">
+
           <Navbar />
           <main>{children}</main>
           <Footer />
